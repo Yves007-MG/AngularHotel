94ef1cba042de7c77c245f8a6a0d5b17fa0bc61d
yves <ramarojaona.yves@gmail.com>
2022-08-25 17:00:03 +0300

    ajout pipe price

--- src/app/hotel-list/hotel-list.component.ts
+++ src/app/hotel-list/hotel-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IHotel } from './hotel';
 
 @Component({
   selector: 'app-hotel-list',
@@ -8,28 +9,28 @@ import { Component, OnInit } from '@angular/core';
 export class HotelListComponent implements OnInit {
 
   public title  = 'Liste hotels';
-  public hotels : any[]=[
+  public hotels : IHotel[]=[
     {
-      id: 1,
+      hotelId: 1,
       hotelName: 'Buea sweet life',
       description: 'Belle vue au bord de la mer',
       price: 230.5,
       imageUrl: 'assets/img/hotel-room.jpg',
     }, {
-      id: 2,
+      hotelId: 2,
       hotelName: 'Marakech',
       description: 'Profitez de la vue sur les montagnes',
       price: 145.5,
       imageUrl: 'assets/img/the-interior.jpg',
     }, {
-      id: 3,
+      hotelId: 3,
       hotelName: 'Abudja new look palace',
       description: 'Séjour complet avec service de voitures',
       price: 120.12,
       imageUrl: 'assets/img/indoors.jpg',
 
     }, {
-      id: 4,
+      hotelId: 4,
       hotelName: 'Cape town city',
       description: 'Magnifique cadre pour votre séjour',
       price: 135.12,
@@ -45,6 +46,8 @@ export class HotelListComponent implements OnInit {
   ngOnInit(): void {
   }
   public showBadge!:boolean ;
+  public hotelFilter = 'mot' ;
+
   public toogleIsNewBadge():void{
     this.showBadge = !this.showBadge;
   }
